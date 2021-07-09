ec1af4b6232330722c2055f1a0ddf1d5c2d6cae0
Aloxan12 <kvakvaaak@mail.ru>
2021-07-09 14:20:32 +0300

    local storage(with useEffect)

--- src/components/Localstorage/counter.tsx
+++ src/components/Localstorage/counter.tsx
@@ -1,22 +1,25 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 export const Counter=()=>{
     const [value, setValue]=useState(0)
 
-    const incHandler=()=>{
-        setValue(value + 1)
-    }
-    const setToLocalStorage=()=>{
-        localStorage.setItem('counterValue', JSON.stringify(value))
-    }
-    const getFromLocalStorage=()=>{
+    useEffect(()=>{
         let valueAsString = localStorage.getItem('counterValue')
         if(valueAsString){
             const newValue = JSON.parse(valueAsString)
             setValue(newValue)
         }
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    },[value])
+
+    const incHandler=()=>{
+        setValue(value + 1)
     }
+
     const clearLocalStorage=()=>{
         localStorage.clear()
         setValue(0)
@@ -25,8 +28,6 @@ export const Counter=()=>{
         <div>
             <h1>{value}</h1>
             <button onClick={incHandler}>+</button>
-            <button onClick={setToLocalStorage}>setToLocalStorage</button>
-            <button onClick={getFromLocalStorage}>getToLocalStorage</button>
             <button onClick={clearLocalStorage}>clearLocalStorage</button>
         </div>
     )
