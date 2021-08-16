7b889323428a24d8d64c44b1b77defc0c8785d65
Aloxan12 <kvakvaaak@mail.ru>
2021-08-16 10:46:58 +0300

    tasks

--- src/common/Vanila/Base/00.js
+++ src/common/Vanila/Base/00.js
@@ -22,69 +22,31 @@
 // const dog = new Animal('Bob', 5)
 // console.log(dog.GetNameAge())
 
-// exampleArray = [{id: 2, title: 'Two'}, {id: 3, title: 'Three'}, {id: 1, title: 'One'}];
-// function mapper(arr) {
-//     const newArr = arr.map(obj => Object.values(obj))
-//     // const obj = newArr.reduce((acc, cur, i)=>
-//     //     acc[i]= cur, {});
-//     return newArr
-// }
-// console.log(mapper(exampleArray)); // { '2': 'Two', '3': 'Three', '1': 'One' }
+exampleArray = [{id: 2, title: 'Two'}, {id: 3, title: 'Three'}, {id: 1, title: 'One'}];
+function mapper(arr) {
+    const newArr = arr.reduce((acc, item)=> ({...acc, [item.id]:item.title}),{})
+    return newArr
+}
+console.log(mapper(exampleArray)); // { '2': 'Two', '3': 'Three', '1': 'One' }
+
+
 // const arr = [2, 2, 2, 3] // [7, 5, 8]
-//
 // const sumArr = (arr)=>{
 //     const newArr = arr.map((n, index)=> arr.filter((num, index2)=> index2 !==index)).map(ar => ar.reduce((acc, el)=> acc+ el, 0))
 //     return newArr
 // }
-// const sumArr2 = (arr)=>{
-//     let newArr = 0
-//     for(let i=0; i < arr.length; i++){
-//         newArr = arr.reduce((acc, el)=> acc+ el, 0)
-//     }
-//     return newArr;
-// }
-//
-//
 // console.log(sumArr(arr))
 
 
-
-
-const arr = [
-    { id: [{ name: "1" }, { name: "2" }], num: "1" },
-    { id: [{ name: "0" }], num: "2" },
-    { id: [{ name: "1" }], num: "3" },
-    { id: [{ name: "1" }, { name: "2" }], num: "4" },
-    { id: [{ name: "1" }, { name: "2" }], num: "5" }
-];
-
-function callArr(arr) {
-    const newArr = arr.map((obj, index) => {
-        const filteredObj = obj.id.filter(ar => ar.name === '2')
-        return filteredObj
-    })
-    return newArr
-}
-
-console.log(callArr(arr));
-
-// console.log(callArr(arr));
-// function callArr(arr){
-//     const newArr = arr.map((obj, index)=> {
-//         for (let key in obj) {
-//             return obj[key]
-//         }
-//     }).map((ar, i) => ar.filter(a => a.name === '2'))
-//     return newArr
-// }
-// function callArr2(arr){
-//     const result = []
-//     const newArr = arr.map(obj =>{
-//         for (let key in obj) {
-//             return obj[key].filter((ar, i) => ar[i].name === '2')
-//         }
-//     })
+// const arr = [
+//     {id: [{name: "1"}, {name: "2"}], num: "1"},
+//     {id: [{name: "0"}], num: "2"},
+//     {id: [{name: "1"}], num: "3"},
+//     {id: [{name: "1"}, {name: "2"}], num: "4"},
+//     {id: [{name: "1"}, {name: "2"}], num: "5"}
+// ];
+// function callArr(arr) {
+//     const result = arr.filter(obj => obj.id.some(ar => ar.name === "2"))
 //     return result
 // }
-
-console.log(callArr(arr))
+// console.log(callArr3(arr));
--- src/common/Vanila/Base/01.ts
+++ src/common/Vanila/Base/01.ts
@@ -18,17 +18,36 @@ const arr: Array<objType> = [
     {id: [{name: "1"}, {name: "2"}], num: "5"}
 ];
 
-function callArr(arr: arrType) {
-    const newArr = arr.map((obj, i)=>{
-        if(arr[i].id){
+// function callArr(arr: arrType) {
+//     const result: any = []
+//     const newArr = arr.map((obj, i)=>{
+//         if(arr[i].id[0].name === "2"){
+//             result.push(arr[i])
+//         }
+//     })
+//     // const newArr = arr.map((obj, index) => {
+//     //     const filteredObj = obj.id.filter((ar, i) => ar.name === '2'? obj : null)
+//     //     return filteredObj
+//     // })
+//     return result
+// }
 
-        }
-    })
-    // const newArr = arr.map((obj, index) => {
-    //     const filteredObj = obj.id.filter((ar, i) => ar.name === '2'? obj : null)
-    //     return filteredObj
-    // })
-    return
+
+// function callArr2(arr: arrType) {
+//     const result:any = []
+//     for (let i = 0; i < arr.length; i++) {
+//         const filt = arr[i].id.filter(ob => ob.name === "2")
+//         if(filt){
+//             result.push(filt)
+//         }
+//     }
+//     return result
+// }
+function callArr3(arr: arrType) {
+    const result = arr.filter(obj=> obj.id.filter(ar=>ar.name === "2"))
+
+    return result
 }
 
-console.log(callArr(arr));
+
+console.log(callArr3(arr));
