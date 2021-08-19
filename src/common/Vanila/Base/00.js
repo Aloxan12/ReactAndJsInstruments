e6127f209dc457f6789a572ad2948891bca83baf
Aloxan12 <kvakvaaak@mail.ru>
2021-08-19 11:06:04 +0300

    tasks

--- src/common/Vanila/Base/00.js
+++ src/common/Vanila/Base/00.js
@@ -48,28 +48,43 @@
 //     return result
 // }
 // console.log(callArr3(arr));
-
-const array1 = ["one"];
-const array2 = ["two", "three"];
-const array3 = ["one", "two", "three", "six"];
-const array4 = [];
-
-const obj = {
-    one: ["testOne1", "testOne2"],
-    two: ["testTwo1", "testTwo2"],
-    three: ["testThree1", "testThree2"],
-    four: ["testFour1", "testFour1"],
-    five: ["testFive1", "testFive1"],
-    six: ["testSix1", "testSix2"],
-    seven: ["testSeven1", "testSeven1"]
-}
-
-const myFunc = (arr, data) => {
-    const newArr2 = arr.reduce((acc, item)=> [...acc, data[item]], []).reduce((acc, item)=> [...acc, ...item], [])
-    return newArr2
-};
-
+// const array1 = ["one"];
+// const array2 = ["two", "three"];
+// const array3 = ["one", "two", "three", "six"];
+// const array4 = [];
+//
+// const obj = {
+//     one: ["testOne1", "testOne2"],
+//     two: ["testTwo1", "testTwo2"],
+//     three: ["testThree1", "testThree2"],
+//     four: ["testFour1", "testFour1"],
+//     five: ["testFive1", "testFive1"],
+//     six: ["testSix1", "testSix2"],
+//     seven: ["testSeven1", "testSeven1"]
+// }
+// const myFunc = (arr, data) => {
+//     const newArr2 = arr.reduce((acc, item)=> [...acc, data[item]], []).reduce((acc, item)=> [...acc, ...item], [])
+//     return newArr2
+// };
 //console.log(myFunc(array1, obj)); // return ['testOne1', 'testOne2']
-console.log(myFunc(array2, obj)); // return ['testTwo1', 'testTwo2', 'testThree1', 'testThree2']
+//console.log(myFunc(array2, obj)); // return ['testTwo1', 'testTwo2', 'testThree1', 'testThree2']
 //console.log(myFunc(array3, obj)); // return ['testOne1', 'testOne2', 'testTwo1', 'testTwo2', 'testThree1', 'testThree2', 'testSix1', 'testSix2']
 //console.log(myFunc(array4, obj)); // return []
+
+
+let strin = 'Lets go to the movies'
+let strin2 = 'Why isn\'t my code working?'
+
+
+function format(str) {
+    const s = str.length;
+    const chars = str.split('');
+    const strWithSpaces = chars.reduceRight((acc, char, i) => {
+        const spaceOrNothing = ((((s - i) % 1) === 0) ? ' ' : '');
+        return (spaceOrNothing + char + acc);
+    }, '');
+    let result = ((strWithSpaces[0] === ' ') ? strWithSpaces.slice(1) : strWithSpaces).toUpperCase();
+    return result.replace(/ +/g, ' ').trim()
+}
+
+console.log(format(strin2))
