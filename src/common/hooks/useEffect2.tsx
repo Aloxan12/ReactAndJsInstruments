7006f1da1878ee9859c229654d6b1932c98eb4b6
Aloxan12 <kvakvaaak@mail.ru>
2021-08-23 21:29:44 +0300

    useEffect(2)

--- src/common/hooks/useEffect2.tsx
+++ src/common/hooks/useEffect2.tsx
@@ -9,26 +9,39 @@ type SearchUserType = {
 type SearchResult = {
     items: SearchUserType[]
 }
+type UserType = {
+    login: string
+    id: number
+    avatar_url: string
+    followers: number
+}
 
 export const Github = () => {
     const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
+    const [userDetails, setUserDetails] = useState<UserType | null>(null)
     const [usersGH, setUsersGH] = useState<SearchUserType[]>([])
     const [tempSearch, setTempSearch] = useState('it-kamasutra')
-    const [searchTerm, setSearchTerm] = useState('it-kamasutra')
+    const [searchTerm, setSearchTerm] = useState('it')
 
     useEffect(() => {
         if (selectedUser) {
             document.title = selectedUser.login
         }
     }, [selectedUser])
-
-
     useEffect(() => {
         axios.get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
             .then(res => {
                 setUsersGH(res.data.items)
             })
     }, [searchTerm])
+    useEffect(() => {
+        if (!!selectedUser) {
+            axios.get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
+                .then(res => {
+                    setUserDetails(res.data)
+                })
+        }
+    }, [selectedUser])
 
 
     return (
@@ -46,7 +59,7 @@ export const Github = () => {
                 </div>
                 <ul>
                     {usersGH
-                        ?.map((u) => <li
+                        ?.map((u) => <li key={u.id}
                             className={selectedUser === u ? s.selected : ''}
                             onClick={() => {
                                 setSelectedUser(u)
@@ -57,7 +70,11 @@ export const Github = () => {
             </div>
             <div>
                 <h2>Username</h2>
-                <div>Details</div>
+                {userDetails && <div>
+                    <img src={userDetails.avatar_url} alt={'Avatar'}/>
+                    <br/>
+                    {userDetails.login}, followers:{userDetails.followers}
+                </div>}
             </div>
         </div>
     )
