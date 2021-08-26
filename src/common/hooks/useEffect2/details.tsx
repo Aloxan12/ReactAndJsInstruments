654b9e038995286a8a811ea2d02eb2e386e46b48
Aloxan12 <kvakvaaak@mail.ru>
2021-08-26 10:23:50 +0300

    useEffect(2) Timer

--- src/common/hooks/useEffect2/details.tsx
+++ src/common/hooks/useEffect2/details.tsx
@@ -9,31 +9,65 @@ export type UserType = {
     avatar_url: string
     followers: number
 }
-
 type DetailsType = {
     selectedUser: SearchUserType | null
 }
+const startTimerValue = 10
 
 export const Details: React.FC<DetailsType> = ({selectedUser}) => {
     const [userDetails, setUserDetails] = useState<UserType | null>(null)
+    const [seconds, setSeconds] = useState(startTimerValue)
 
     useEffect(() => {
         if (!!selectedUser) {
             axios.get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
                 .then(res => {
+                    setSeconds(startTimerValue)
                     setUserDetails(res.data)
                 })
         }
     }, [selectedUser])
 
+    useEffect(()=>{
+        if(seconds< 1){
+            setUserDetails(null)
+        }
+    },[seconds])
+
     return (
         <>
             <h2>Username</h2>
             {userDetails && <div>
+                <Timer seconds={seconds} onChange={setSeconds} timerKey={userDetails.id}/>
                 <img src={userDetails.avatar_url} alt={'Avatar'}/>
                 <br/>
                 {userDetails.login}, followers:{userDetails.followers}
             </div>}
         </>
+    )
+}
+type TimerType={
+    seconds: number
+    onChange:(value: number)=>void
+    timerKey: number
+}
+export const Timer:React.FC<TimerType> =({seconds, onChange, timerKey})=>{
+    const [second, setSecond] = useState(seconds)
+    useEffect(()=>{
+        setSecond(seconds);
+    },[seconds])
+
+    useEffect(()=>{
+        onChange(second)
+    },[second])
+
+    useEffect(()=>{
+        const setIntervalId = setInterval(()=>{
+            setSecond((prev)=> prev - 1)
+        },1000)
+        return ()=> clearInterval(setIntervalId)
+    },[timerKey])
+    return (
+        <div>{second}</div>
     )
 }
