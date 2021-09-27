d1733c5f7aab8b62ee56b8e21e3672c55af2a791
Aloxan12 <kvakvaaak@mail.ru>
2021-09-27 22:36:51 +0300

    Dependency inversion

---
+++ src/common/Vanila/SOLID/5_D.js
@@ -0,0 +1,69 @@
+//Dependency inversion — принцип инверсии зависимостей
+//Модули верхних уровней не должны зависеть от модулей нижних уровней,
+// а оба типа модулей должны зависеть от абстракций;
+// сами абстракции не должны зависеть от деталей, а вот детали должны зависеть от абстракций.
+
+
+class Fetch {
+    request(url){
+        //return fetch(url).then(r => r.json())
+        return Promise.resolve('data from fetch')
+    }
+}
+class LocalStorage {
+    get(){
+        const dataFromLocalStorage = 'data from local storage'
+        return dataFromLocalStorage
+    }
+}
+
+class FetchClient{
+    constructor() {
+        this.fetch = new Fetch()
+    }
+    clientGet(){
+        return this.fetch.request('vk.com')
+    }
+}
+
+class LocalStorageClient{
+    constructor() {
+        this.localStorage = new LocalStorage()
+    }
+    clientGet(){
+        return this.localStorage.get('ls key')
+    }
+}
+
+class Database{
+    constructor(client) {
+        //this.fetch = new Fetch() --
+        //this.localStorage = new LocalStorage() --
+        this.client = client //++
+    }
+    getData(key){
+        //return this.fetch.request('vk.com') --
+        //return this.localStorage.get('ls key ') --
+        return this.client.clientGet(key)
+    }
+}
+
+// const db = new Database() --
+// console.log(db.getData()) --
+const db = new Database(new FetchClient())
+console.log(db.getData('rand'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
