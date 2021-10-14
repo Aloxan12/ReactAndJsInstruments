004065a5d47e910ddb9710663748eba4be9b5462
Aloxan12 <kvakvaaak@mail.ru>
2021-10-15 00:08:23 +0300

    test

---
+++ src/common/Vanila/Base/000.js
@@ -0,0 +1,13 @@
+const arr = [1,2,4,8,5,6]
+
+const maxArr = Math.max(...arr)
+
+console.log(maxArr)
+
+const sum = arr.reduce((acc, num)=> +acc + num, [])
+console.log(sum)
+
+const summm=()=>{
+    return this
+}
+console.log(summm())
