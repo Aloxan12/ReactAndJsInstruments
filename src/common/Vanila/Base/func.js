7adfa1255ff76419256087908851df06fad81212
Aloxan12 <kvakvaaak@mail.ru>
2021-11-04 14:52:52 +0300

    fn

--- src/common/Vanila/Base/func.js
+++ src/common/Vanila/Base/func.js
@@ -42,23 +42,23 @@
 //     }
 // }
 
-let obj = {
-    name: 'Alex',
-    f(){
-        setTimeout(()=>{
-            console.log('arrow', this) //Alex
-        }, 0);
-        setTimeout(function (){
-            console.log('FD', this) //Window
-        }, 0)
-    }
-}
-obj.f();
+// let obj = {
+//     name: 'Alex',
+//     f(){
+//         setTimeout(()=>{
+//             console.log('arrow', this) //Alex
+//         }, 0);
+//         setTimeout(function (){
+//             console.log('FD', this) //Window
+//         }, 0)
+//     }
+// }
+// obj.f();
 
 // let obj = {
 //     name: 'Evgen',
 //     sayName(a1, a2){
-//         console.log(this.name, a1, a2)
+//         console.log(this.name, a1)
 //     }
 // }
 // let obj2 = {
@@ -69,3 +69,20 @@ obj.f();
 // }
 // let result = obj.sayName.bind(obj3, 100).bind(obj2, 200)
 // result();
+
+const person = {
+    name: 'Alex',
+    age: 26,
+    sayHello: () => {
+        console.log('Arrow ' + this.name)
+    }
+}
+console.log(person.sayHello())
+const person2 = {
+    name: 'Alex',
+    age: 26
+}
+// const arrFn =()=>{
+//     return console.log('Arrow' + this.name)
+// }
+
