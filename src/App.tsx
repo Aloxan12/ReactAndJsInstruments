d57d1e02683261b9b42b75ee9a1cb020a9b17e57
Aloxan12 <kvakvaaak@mail.ru>
2021-11-21 01:50:32 +0300

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import {HookRef} from "./common/hooks/useRef/useRef";
 
 
 function App() {
 
     return (
         <div className="App">
-            po
+            <HookRef />
         </div>
     );
 }
--- src/common/hooks/useRef/useRef.tsx
+++ src/common/hooks/useRef/useRef.tsx
@@ -1,13 +1,28 @@
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 
 export const HookRef = ()=>{
+    const [value, setValue] = useState('value')
+    const renderRef = useRef(0)
+    const inputRef = useRef<null | any>(null)
+    const prevValueRef = useRef<null | any>(null)
 
-    const ref = useRef(null)
+    useEffect(()=>{
+        renderRef.current++;
+    })
+
+    useEffect(()=>{
+        prevValueRef.current = value;
+    }, [value])
+
+    const focus =()=> inputRef.current.focus();
 
     return (
         <div>
-            <input ref={ref} type="text"/>
+            <div>Количество рендеров: {renderRef.current}</div>
+            <div>Предыдущее value: {prevValueRef.current}</div>
+            <input ref={inputRef} value={value} onChange={(e)=>setValue(e.currentTarget.value)} type="text"/>
+            <button onClick={focus}>Фокус</button>
         </div>
     )
 }
