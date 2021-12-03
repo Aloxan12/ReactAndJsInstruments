02c5b86d81cd5118dda8270b16c8f26d33b05afa
Aloxan12 <kvakvaaak@mail.ru>
2021-12-04 00:46:13 +0300

    style navLink

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import "./Header.css";
 
 
 export const Header = () => {
     return (
-        <div>
-            <ul>
-                <li><NavLink to='/'>Home</NavLink></li>
-                <li><NavLink to='/Clock'>Clock</NavLink></li>
-            </ul>
+        <div className="wrap">
+            <div className="header-component">
+                <ul>
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/Clock'>Clock</NavLink></li>
+                </ul>
+            </div>
         </div>
     )
 }
