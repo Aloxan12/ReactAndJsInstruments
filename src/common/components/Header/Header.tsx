31d5e583b9482c5f8ce0ed03d1ef86541169e56e
Aloxan12 <kvakvaaak@mail.ru>
2021-12-06 00:26:44 +0300

    test

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -10,6 +10,7 @@ export const Header = () => {
                 <ul>
                     <li><NavLink to='/'>Home</NavLink></li>
                     <li><NavLink to='/Clock'>Clock</NavLink></li>
+                    <li><NavLink to='/Clock'>Clock</NavLink></li>
                 </ul>
             </div>
         </div>
