73bb9a7c21010f166ce187e19fec561d4686f716
Aloxan12 <kvakvaaak@mail.ru>
2021-12-18 00:42:03 +0300

    TableFlex

--- src/common/components/Tables/TableFlex.tsx
+++ src/common/components/Tables/TableFlex.tsx
@@ -21,20 +21,6 @@ export const TableFlex = () => {
                 </div>
             </div>
 
-            <div className="wrapper">
-                <div className="box">
-                    <div>One</div>
-                    <div>Two</div>
-                    <div>Three</div>
-                    <div>Four</div>
-                    <div>Five</div>
-                    <div>Six</div>
-                    <div>Seven</div>
-                    <div>Eight</div>
-                    <div>Nine</div>
-                    <div>Ten</div>
-                </div>
-            </div>
         </div>
     )
 }
