4c5db9eb8575a7756e50a2f91b9744b9b3da8a28
Aloxan12 <kvakvaaak@mail.ru>
2021-12-26 00:37:26 +0300

    UploadFile

--- src/common/DragAndDrop/Test.tsx
+++ src/common/DragAndDrop/Test.tsx
@@ -4,19 +4,31 @@ import DocViewer, {DocViewerRenderers} from "react-doc-viewer";
 import {Modal, ModalView} from "../Modal/Modal";
 
 export const Test = () => {
-    const [active, setActive] = useState(false)
-    const docs = [
+    const [docs, setDocs] = useState( [
         {uri: "https://c8261d96-4316-475f-9f68-c7d397d4d0b5.selcdn.net/media/Kitayskiy.docx"},
         {uri: "https://c8261d96-4316-475f-9f68-c7d397d4d0b5.selcdn.net/media/square_320_c09ebae17387b7d6eeb9fa0d42afe5ee.jpg"}
-    ]
-
-    return(
+    ])
+    const handleChangeFile = (files: any) => {
+        const file = files[0]
+        if (file) {
+            const formData = new FormData()
+            formData.append('file', file)
+            setDocs(prevValue => [...prevValue, {uri: file}])
+        }
+    }
+    return (
         <div style={{height: '100vh'}}>
-            <button onClick={() => setActive(true)}>Изменить
-                тренера</button>
-            <Modal active={active} setActive={setActive}>
-            </Modal>
-            <DocViewer style={{width:"600px", height: '700px', margin: 'auto'}} pluginRenderers={DocViewerRenderers} documents={docs} />
+            <input
+                onChange={(e) => handleChangeFile(e.target.files)}
+                type="file"
+                name="file"
+                id="file"
+                className="input_file"
+            />
+            <div>
+                <DocViewer style={{width: "600px", height: '700px', margin: 'auto'}}
+                           pluginRenderers={DocViewerRenderers} documents={docs}/>
+            </div>
         </div>
     )
 }
