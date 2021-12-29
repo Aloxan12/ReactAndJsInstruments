5c6483f72d14b19f1437ad363e8bc67385b835cb
Aloxan12 <kvakvaaak@mail.ru>
2021-12-30 00:01:55 +0300

    UploadFile

---
+++ src/common/Typescript/Typescript.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+interface IComponent {
+    title: string
+}
+
+type ComponentType = {
+    title: string
+}
+
+export const Component: React.FC<ComponentType> = ({title}) =>{
+    return (
+        <div>
+            <span>{title}</span>
+        </div>
+    )
+}
