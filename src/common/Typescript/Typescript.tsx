b45baf55bb6992c490fc585e6c780c7330301da3
Aloxan12 <kvakvaaak@mail.ru>
2022-01-07 18:54:32 +0300

    mobx

--- src/common/Typescript/Typescript.tsx
+++ src/common/Typescript/Typescript.tsx
@@ -14,6 +14,7 @@ export const Component: React.FC<ComponentType> = ({title}) =>{
     return (
         <div>
             <span>{title}</span>
+            <span>{title}</span>
         </div>
     )
 }
