cf97f8dfadd6593074616f2f926b826dc60ff693
Aloxan12 <kvakvaaak@mail.ru>
2022-06-08 12:57:28 +0300

    scope

--- src/common/Vanila/scope.js
+++ src/common/Vanila/scope.js
@@ -1,7 +1,14 @@
-var item = 'item'
-const fn =()=>{
-    var item = 'lol'
-    console.log('item', item) //lol
-}
-console.log('item', item) //item
-fn()//lol
+// var item = 'item'
+// const fn =()=>{
+//     var item = 'lol'
+//     console.log('item', item) //lol
+// }
+// console.log('item', item) //item
+// fn()//lol
+
+
+
+greeting = 'hello'
+console.log(greeting) // 'hello'
+var greeting = 'hi'
+console.log(greeting)// hi
