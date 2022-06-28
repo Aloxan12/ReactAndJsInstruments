cc84285d933e1b4becde8991208955fde1ff90b4
Aloxan12 <kvakvaaak@mail.ru>
2022-06-28 16:55:16 +0300

    async

--- src/common/Vanila/generate.js
+++ src/common/Vanila/generate.js
@@ -1,6 +1,11 @@
-function *foo(x, y){
-    return x * y
+function *foo(x){
+    let res = x * (yield)
+    return res
 }
-const it = foo(6,7)
+const it = foo(6)
+it.next()
+const res = it.next(6)
 
-console.log(it.next())
+console.log(res)
+
+
